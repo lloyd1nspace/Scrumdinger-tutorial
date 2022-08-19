78ce219946b368d8d78f067f84461006e506b1b0
Lloyd W. Sykes <lsykes@etsy.com>
2022-08-19 11:02:49 -0400

    Swift UI Essentials - Using Stacks to Arrange Views

---
+++ Scrumdinger/Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Lloyd W. Sykes on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
